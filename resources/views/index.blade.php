bb5c044b37427ad70e289dcd29ebf19ad1743daa
darkcount2011 <46963174+darkcount2011@users.noreply.github.com>
2021-06-06 17:36:21 +0200

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,40 @@
+<div>
+
+    <p>
+    <ul id="sideline">
+        Quick links
+        <li><a href=#ict>Why HBO-ICT?</a></li>
+        <li><a href=#personality>Personality</a></li>
+
+
+
+    </ul>
+</div>
+
+
+<div id="wrapper">
+    <p id="ict" class="headertext">Why HBO-ICT?</p>
+    <p class="middletextfield"> I used to always think that I had to go to a university when I was still doing VWO because you will get alot of information on top level.
+        I later noticed that I am someone that likes to put the things I learned into practice just so I can keep my head around it while staying busy and concentrating on the subject.
+        My interest in ICT went up ones I got around the age of 12 when I met people online that were able to DDoS me in the middle of a Call of Duty match. And I wondered how people
+        can make things like that. After that I got interested in game engines and tried some easy 2D platform engine. I made my art project out of it with a friend and it was really fun to do!
+        I started to learn a bit of css/html in secondary school when one of my classes was Informatica. I always thought it was fun to do unlike my other classes like chemistry or physics.
+        It gives a bit of freedom which I like, and you can use your own way to accomplish something instead of staying true to a specific ruleset like in mathematics.
+        Like here you have multiple ways to make a site look good while in mathematics they teach you 1 way and you must use it on your test / exam.
+
+    </p>
+
+    <p class="middletextfield"> Though I didn't do any coding at home or anything. I had a project
+        with a friend of mine (Elwin) for an ice-skate company that sets up a place to skate in the winter here in Middelburg at the market.
+        We had to log alot of things from ice temperature, weather temperature, air humidity to how many km/h the wind was blowing and from what angle.
+        We made a site to log it and used an API with weather forecasts to predict the ice temperature and how to prevent it from either becoming too hard or make it a swimming pool.
+        It was a challenging project and it took alot of effort since we constantly had to learn new things while working hard to meet the deadline. But when we managed to get our project done
+        I just knew I wanted to do something with projects based around computers / coding.
+    </p>
+    <p id="personality" class="headertext">Why it fits around my personality</p>
+    <p class="middletextfield"> I am not introverted and like to work alongside people. I am able to concentrate for a long time and put alot of effort and energy in something.
+        Working with others makes me try to do even more because others count on me and I like the responsibility I get from that. I hate deadlines though but they push me to get a schedule
+        together for a more structured and linear progression towards my goal. I don't mind communicating and I know communication is important while doing work for a client.
+
+    </p>
+</div>
