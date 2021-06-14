806fa6654fea0f80f06f0895504e7e482c722a39
darkcount2011 <46963174+darkcount2011@users.noreply.github.com>
2021-06-14 23:35:09 +0200

    storiesfix

--- resources/views/stories/index.blade.php
+++ resources/views/stories/index.blade.php
@@ -9,8 +9,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <a type="button" class="btn btn-block btn-primary" href="{{route('adminGroups.create')}}">Maak
-                        Groep</a>
+                    <a type="button" class="btn btn-block btn-primary" href="{{route('stories.create')}}">make
+                        a beautiful story</a>
                     <div class="row">
                         @foreach($stories as $story)
                             <div class="col-4">
