255248a283c2193c0d65608823df1968051b1bd1
darkcount2011 <46963174+darkcount2011@users.noreply.github.com>
2021-06-19 23:58:30 +0200

    500 fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -84,14 +84,13 @@
         </a>
         <header class="bg-white shadow">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                {{ $header }}
             </div>
         </header>
 
 
         <main class="py-4">
             @yield('content')
-             {{ $slot }}
+
         </main>
     </div>
 </body>
